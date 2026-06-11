e0a910bccda7946197bc25b7ccddb06c6c13339a
abram <2080084609@qq.com>
2026-06-11 09:24:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "gradplan-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 有网：取最新版并更新缓存；断网：用本地缓存
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
